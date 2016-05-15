6427c7025b6cab447d021a7be04a1c8c8cb08ac2
Fenikz <dsmakarov@gmail.com>
2016-05-15 20:30:29 +0300

    Добавлен SearchView

--- app/src/main/java/com/makbeard/githubuserfinder/activities/MainActivity.java
+++ app/src/main/java/com/makbeard/githubuserfinder/activities/MainActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.SearchView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.jakewharton.rxbinding.widget.RxSearchView;
 import com.jakewharton.rxbinding.widget.RxTextView;
 import com.jakewharton.rxbinding.widget.TextViewTextChangeEvent;
 import com.makbeard.githubuserfinder.GitHubApi;
@@ -23,22 +29,17 @@ import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnTextChanged;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import rx.Observable;
-import rx.Observer;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
-import rx.subjects.PublishSubject;
-import rx.subscriptions.CompositeSubscription;
 
 import static java.lang.String.format;
 
@@ -46,13 +47,14 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MA_MAKTAG";
 
-    @BindView(R.id.search_edittext)
-    EditText mSearchEditText;
+    @BindView(R.id.search_edittext) EditText mSearchEditText;
+    SearchView mSearchView;
 
     private Subscription mSubscription;
     private List<GitUser> mGitUsersList = new ArrayList<>();
     private UsersRecyclerViewAdapter mRecyclerViewAdapter;
     private RecyclerView mRecyclerView;
+    private GitHubApi mGitHubApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +62,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
 
         //Создаём interceptor для анализа логов
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
@@ -80,12 +87,15 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
-        final GitHubApi gitHubApi = retrofit.create(GitHubApi.class);
+        mGitHubApi = retrofit.create(GitHubApi.class);
 
         mRecyclerViewAdapter = new UsersRecyclerViewAdapter(mGitUsersList);
         mRecyclerView = (RecyclerView) findViewById(R.id.gitusers_recyclerview);
         mRecyclerView.setAdapter(mRecyclerViewAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
 
         //Создаём слушателя для поля ввода
         mSubscription = RxTextView.textChangeEvents(mSearchEditText)
@@ -99,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 .doOnNext(new Action1<TextViewTextChangeEvent>() {
                     @Override
                     public void call(TextViewTextChangeEvent textViewTextChangeEvent) {
-                        gitHubApi.getUsersList(textViewTextChangeEvent.text().toString())
+                        mGitHubApi.getUsersList(textViewTextChangeEvent.text().toString())
                                 .subscribeOn(Schedulers.newThread())
                                 .observeOn(AndroidSchedulers.mainThread())
                                 .subscribe(getUsersSubscriber());
@@ -138,5 +148,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mSubscription.unsubscribe();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        mSearchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        RxSearchView.queryTextChanges(mSearchView)
+                .debounce(1000, TimeUnit.MILLISECONDS)
+                .filter(new Func1<CharSequence, Boolean>() {
+                    @Override
+                    public Boolean call(CharSequence charSequence) {
+                        return !TextUtils.isEmpty(charSequence);
+                    }
+                })
+                .doOnNext(new Action1<CharSequence>() {
+                    @Override
+                    public void call(CharSequence charSequence) {
+                        mGitHubApi.getUsersList(charSequence.toString())
+                                .subscribeOn(Schedulers.newThread())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(getUsersSubscriber());
+                    }
+                })
+                .subscribe();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
     }
 }
