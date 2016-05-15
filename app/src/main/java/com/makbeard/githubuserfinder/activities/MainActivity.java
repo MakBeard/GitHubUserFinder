dbe995f7094b76b15288f17fd78a7e654cbdeb21
Fenikz <dsmakarov@gmail.com>
2016-05-15 17:14:04 +0300

    Обращение к API убрано в слушатель кнопки

--- app/src/main/java/com/makbeard/githubuserfinder/activities/MainActivity.java
+++ app/src/main/java/com/makbeard/githubuserfinder/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -14,23 +13,26 @@ import com.makbeard.githubuserfinder.GitHubApi;
 import com.makbeard.githubuserfinder.R;
 import com.makbeard.githubuserfinder.model.GitUser;
 import com.makbeard.githubuserfinder.model.RootUsersResponse;
-import com.quinny898.library.persistentsearch.SearchBox;
 
 import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 import rx.subscriptions.CompositeSubscription;
 
 import static java.lang.String.format;
@@ -42,8 +44,7 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.search_edittext)
     EditText mSearchEditText;
 
-    private CompositeSubscription mSubscriptions;
-
+    private Subscription mSubscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +52,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-
-        mSubscriptions = new CompositeSubscription();
-
-        mSubscriptions.add(RxTextView.textChangeEvents(mSearchEditText)
-                .debounce(1000, TimeUnit.MILLISECONDS)
-                .subscribe(getSearchObserver())
-        );
 
         //Создаём interceptor для анализа логов
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
@@ -77,11 +71,26 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
-        GitHubApi gitHubApi = retrofit.create(GitHubApi.class);
-        mSubscriptions.add(gitHubApi.getUsersList("makbeard")
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(getUsersSubscriber()));
+        final GitHubApi gitHubApi = retrofit.create(GitHubApi.class);
+
+        mSubscription = RxTextView.textChangeEvents(mSearchEditText)
+                .debounce(1000, TimeUnit.MILLISECONDS)
+                .filter(new Func1<TextViewTextChangeEvent, Boolean>() {
+                    @Override
+                    public Boolean call(TextViewTextChangeEvent textViewTextChangeEvent) {
+                        return textViewTextChangeEvent.text().length() > 0;
+                    }
+                })
+                .doOnNext(new Action1<TextViewTextChangeEvent>() {
+                    @Override
+                    public void call(TextViewTextChangeEvent textViewTextChangeEvent) {
+                        gitHubApi.getUsersList(textViewTextChangeEvent.text().toString())
+                                .subscribeOn(Schedulers.newThread())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(getUsersSubscriber());
+                    }
+                })
+                .subscribe();
     }
 
     private Subscriber<RootUsersResponse> getUsersSubscriber() {
@@ -105,28 +114,9 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    private Observer<TextViewTextChangeEvent> getSearchObserver() {
-        return new Observer<TextViewTextChangeEvent>() {
-            @Override
-            public void onCompleted() {
-                Log.d(TAG, "onCompleted: ");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError: ");
-            }
-
-            @Override
-            public void onNext(TextViewTextChangeEvent onTextChangeEvent) {
-                Log.d(TAG, "onNext: " + format("Searching for %s", onTextChangeEvent.text().toString()));
-            }
-        };
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mSubscriptions.unsubscribe();
+        mSubscription.unsubscribe();
     }
 }
