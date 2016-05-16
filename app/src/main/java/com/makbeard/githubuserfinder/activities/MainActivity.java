aeb86e1c1729ba0cc1b8e24a3123dae493447e26
Fenikz <dsmakarov@gmail.com>
2016-05-16 23:13:24 +0300

    Добавлен setOnSuggestionListener

--- app/src/main/java/com/makbeard/githubuserfinder/activities/MainActivity.java
+++ app/src/main/java/com/makbeard/githubuserfinder/activities/MainActivity.java
@@ -1,14 +1,18 @@
 package com.makbeard.githubuserfinder.activities;
 
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
@@ -34,15 +38,14 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
-import rx.Observer;
-import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SearchView.OnSuggestionListener {
 
     private static final String TAG = "MA_MAKTAG";
 
@@ -65,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.searchview)
     SearchView mSearchView;
+    private SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,46 +79,51 @@ public class MainActivity extends AppCompatActivity {
 
         mGitHubApi = initRetrofit();
 
-        //Создаём Observable из кастомного SearchBox
-        Observable<String> searchBoxObservable =
-                Observable.create(new rx.Observable.OnSubscribe<String>() {
-            @Override
-            public void call(final Subscriber<? super String> subscriber) {
-                mFloatingSearchView.setOnQueryChangeListener(
-                        new FloatingSearchView.OnQueryChangeListener() {
-                    @Override
-                    public void onSearchTextChanged(String oldQuery, String newQuery) {
-                        subscriber.onNext(newQuery);
-                    }
-                });
-            }
-        });
+        String[] columnNames = {"_id", "text"};
+        final MatrixCursor cursor = new MatrixCursor(columnNames);
 
-        final List<GitSearchSuggestion> gitSearchSuggestionList = new ArrayList<>();
+        String[] array = {"Bauru", "Sao Paulo", "Rio de Janeiro",
+                "Bahia", "Mato Grosso", "Minas Gerais",
+                "Tocantins", "Rio Grande do Sul"};
 
-        mFloatingSearchView.setOnFocusChangeListener(
-                new FloatingSearchView.OnFocusChangeListener() {
-            @Override
-            public void onFocus() {
+        String[] temp = new String[2];
+        int id = 0;
+        for(String item : array){
+            temp[0] = Integer.toString(id++);
+            temp[1] = item;
+            cursor.addRow(temp);
+        }
 
-                //Выпадающий список
-                GitSearchSuggestion gitSearchSuggestion;
-                for(int i=0; i<10; i++){
-                    gitSearchSuggestion = new GitSearchSuggestion("User " + i);
-                    gitSearchSuggestion.setIsHistory(true);
-                    gitSearchSuggestionList.add(gitSearchSuggestion);
-                }
+        final String[] from = new String[] {"text"};
+        final int[] to = new int[] {android.R.id.text1};
 
-                mFloatingSearchView.swapSuggestions(gitSearchSuggestionList);
-            }
+        mAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
+                null,
+                from,
+                to,
+                0);
 
-            @Override
-            public void onFocusCleared() {
+        // TODO: 16.05.2016 Перенести SearchView в Menu
+        mSearchView.setSuggestionsAdapter(mAdapter);
 
-            }
-        });
+//        Observable<String> suggestionObservable =
 
-        mSubscription = RxSearchView.queryTextChanges(mSearchView)
+        mSearchView.setOnSuggestionListener(this);
+        mSearchView.setQueryHint("Enter User Data");
+
+        /*
+        searchView.setOnQueryTextListener(this);
+        searchAdapter = new SimpleCursorAdapter(this, R.layout.listentry, null, new String[] { "name" }, new int[] { R.id.name_entry }, 0);
+        searchView.setSuggestionsAdapter(searchAdapter)
+
+        Then, in the onQueryTextChanged method I have this:
+
+        searchAdapter.changeCursor(cursor);
+        */
+
+        mSubscription = RxSearchView
+                .queryTextChanges(mSearchView)
                 .debounce(1000, TimeUnit.MILLISECONDS)
                 .filter(new Func1<CharSequence, Boolean>() {
                     @Override
@@ -125,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
                 .doOnNext(new Action1<CharSequence>() {
                     @Override
                     public void call(CharSequence charSequence) {
+                        mAdapter.changeCursor(cursor);
                         // TODO: 16.05.2016 Сохраняем запрос в БД
                         mGitHubApi.getUsersList(charSequence.toString())
                                 .subscribeOn(Schedulers.newThread())
@@ -184,5 +194,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mSubscription.unsubscribe();
+    }
+
+    @Override
+    public boolean onSuggestionSelect(int position) {
+        Cursor c = mAdapter.getCursor();
+        if(c.moveToPosition(position)) {
+            String selectedItem = c.getString(1);
+            Log.d(TAG, "onSuggestionSelect: " + selectedItem);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onSuggestionClick(int position) {
+        return false;
     }
 }
